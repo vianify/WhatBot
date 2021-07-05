4875dbd49e07c21b10c7709f5aa2a1e487db1c87
LanzVian <74493818+LanzVian@users.noreply.github.com>
2021-07-05 11:33:36 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,7 @@
+apt update -y && apt upgrade -y
+pkg install wget && pkg install ffmpeg && pkg install nodejs
+npm i -g cwebp
+npm i -g ytdl 
+npm i
+npm i got
+echo " [NOTE] : Bila Semua Command berhasil di jalankan maka tinggal node index.js"
