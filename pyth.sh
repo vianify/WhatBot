f9c6c94bac4a4444358a6134e84990e32bb65635
LanzVian <rollgaming6@gmail.com>
2021-10-18 21:08:31 +0800

    Update pyth.sh

--- pyth.sh
+++ pyth.sh
@@ -1,1 +1,11 @@
-pkg install python
+#!/usr/bin/bash
+
+apt-get install python
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+pip install --r .txt
+
+echo "[*] All dependencies have been installed, please run the command \"python index.py\" to immediately start the script"
