ec1753b765b52843993602288ac0ef21e7dc7bbb
LanzVian <rollgaming6@gmail.com>
2021-10-20 11:31:52 +0800

    update pyth.sh

--- pyth.sh
+++ pyth.sh
@@ -6,6 +6,6 @@ apt-get install mc
 apt-get install ffmpeg
 apt-get install wget
 apt-get install tesseract
-pip install --r .txt
+pip install --r requirements.txt
 
 echo "[*] All dependencies have been installed, please run the command \"python index.py\" to immediately start the script"
