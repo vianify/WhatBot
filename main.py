a10c2f1dd04d6523232ba25e2d5c6b8f4d791e68
LanzVian <rollgaming6@gmail.com>
2021-10-20 12:27:49 +0800

    update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
 import "/lib/pymain"
+
+def dloader(`indent('.') ? 'self' : ''`):
+  """docstring for dloader"""
+  import pprint; pprint.pprint(class ExampleCase(unittest.TestCase):
+    
+    def test_description(self):
+      # TODO: write code...)
